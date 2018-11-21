bf37132594ff66bddb4333f716212f604e407e1d
Bryan Reed <bryan87reed@gmail.com>
2018-11-21 00:01:24 -0700

    Adds pathlength attr to patrols

--- patrol.js
+++ patrol.js
@@ -4,10 +4,11 @@ function patrolling(patrolObject) {
     var token = patrolObject.token;
     var direction = patrolObject.direction;
     var steps = patrolObject.steps;
+    var pathLength = patrolObject.pathLength;
     
     setInterval(function () {
         if (token.get("status_bluemarker") === false) return;
-        if (stepstaken > 5) {
+        if (stepstaken > pathLength) {
             //Switch directions!
             if (direction == "left" && !token.get("fliph")) {
                 token.set("fliph", true);
@@ -24,7 +25,12 @@ function patrolling(patrolObject) {
 
 on("ready", function () {
     var patrols = [
-        {token: findObjs({ name: "patrolHallway" })[0], direction: "left", steps: -47}
+        {token: findObjs({ name: "patrolHallway" })[0], direction: "left", steps: -47, pathLength: 5},
+        {token: findObjs({ name: "patrolEngine1" })[0], direction: "top", steps: 47, pathLength: 10},
+        {token: findObjs({ name: "patrolEngine2" })[0], direction: "top", steps: -47, pathLength: 10},
+        {token: findObjs({ name: "patrolBridge1" })[0], direction: "top", steps: 47, pathLength: 5},
+        {token: findObjs({ name: "patrolBridge2" })[0], direction: "top", steps: -47, pathLength: 6},
+        {token: findObjs({ name: "patrolBridge3" })[0], direction: "top", steps: 47, pathLength: 4}
     ]
     
     for(i=0;i<patrols.length;i++){
